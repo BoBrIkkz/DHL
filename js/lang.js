41403ce2a7cebec4554765db9aa0b32195f76257
BoBrikkz <arsusha02@gmail.com>
2021-05-22 19:10:23 +0300

    translate hero and main section

---
+++ js/lang.js
@@ -0,0 +1,75 @@
+   const langArr = {
+    "item__express" :  {
+        "ru": "Экспресс",
+        "en": "Express",
+    }, 
+    "item__Parcel" :  {
+        "ru": "Посылки & коммерция",
+        "en": "Parcel & Ecommerce",
+    }, 
+    "item__logistics" :  {
+        "ru": "логистика",
+        "en": "logistics",
+    }, 
+    "item__mail" :  {
+        "ru": "почта",
+        "en": "mail",
+    }, 
+    "item__press" :  {
+        "ru": "пресса",
+        "en": "press",
+    }, 
+    "item__about" :  {
+        "ru": "о нас",
+        "en": "about",
+    }, 
+    "Contact__Center" :  {
+        "ru": "Контактный Центр",
+        "en": "Contact Center",
+    }, 
+    "Country__Profile" :  {
+        "ru": "Профиль Страны",
+        "en": " Country Profile",
+    }, 
+    "main__tittle" :  {
+        "ru": "Мы Доставляем<br> Быстро",
+        "en": "We Deliver<br> With Speed",
+    }, 
+    "main__text" :  {
+        "ru": "Когда неизвестный печатник взял что-то и скомкал это чтобы сделать книгу<br>Он пережил более пяти веков",
+        "en": "When an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not only five centuries",
+    }, 
+    "amazing" :  {
+        "ru": "Невероятно",
+        "en": "amazing",
+    }, 
+    "quick" :  {
+        "ru": "Быстро",
+        "en": "quick",
+    }, 
+    "supports" :  {
+        "ru": "24/7 Поддержка",
+        "en": "24/7 supports",
+    }, 
+    "more" :  {
+        "ru": "ЧИТАТЬ БОЛЬШЕ",
+        "en": "LEARN MORE",
+    }, 
+    "worldwide" :  {
+        "ru": "DHL WORLDWIDE",
+        "en": " DHL WORLDWIDE",
+    }, 
+    "" :  {
+        "ru": "",
+        "en": "",
+    }, 
+    "" :  {
+        "ru": "",
+        "en": "",
+    }, 
+    "" :  {
+        "ru": "",
+        "en": "",
+    }, 
+
+}
--- js/main.js
+++ js/main.js
@@ -2,23 +2,48 @@ $(document).ready(function () {
   var searchBlock = $('.header__navigation__search');
   var logisticsBlock = $('.express__card--logistics');
   var expressBlock = $('.express__card');
-  var toggle = $('.toggle');
-  var off = $('.off');
-
+  
+  
   $(document).on('click', '.header__navigation__button', function () {
     searchBlock.slideToggle();
     return false;
   });
-
+  
   $(document).on('click', '.express__logistics__button--toggle', function () {
     expressBlock.removeClass('toggle');
     logisticsBlock.addClass('toggle');
     return false;
   });
-
-    $(document).on('click', '.express__button--toggle', function () {
+  
+  $(document).on('click', '.express__button--toggle', function () {
     expressBlock.addClass('toggle');
     logisticsBlock.removeClass('toggle');
     return false;
   });
+
+  const select = document.querySelector('select');
+  const allLang = ['en', 'ru'];
+  select.addEventListener('change', changeUrlLanguage);
+  function changeUrlLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+  }
+  changeLanguage();
+  function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+      location.href = window.location.pathname + '#en';
+      location.reload();
+      
+    }
+    select.value = hash;
+    for (let key in langArr) {
+      let elem = document.querySelector('.lng-' + key);
+      if (elem) {
+        elem.innerHTML = langArr[key][hash];
+      }
+    }
+  }
 });
